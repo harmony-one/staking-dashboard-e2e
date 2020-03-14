45ac600b4257a230790de9048ec6c53ea345e2d4
yuriy <yuriy.menkov@latoken.com>
2020-03-14 22:15:43 +0300

    update tests

--- test-server.js
+++ test-server.js
@@ -39,7 +39,7 @@ async function execTests() {
 
     addTimeToReport();
 
-    setTimeout(() => execTests(), 30000);
+    setTimeout(() => execTests(), 10 * 60 * 1000);
 }
 
 // Exec tests & reports
